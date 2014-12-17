a798b0a3d79468c133b630c474f765035e550c1a
thavelka <tim.havelka@gmail.com>
2014-12-17 00:19:43 -0600

    Installed Devise and views, implemented users db, cleaned up statuses db.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 	
+  devise_for :users
   resources :statuses
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-	root 'statuses#index'
+	root to: 'statuses#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
