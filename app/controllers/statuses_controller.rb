13f6e7a6c278e50eceb7932d45f2618de22ac6b9
thavelka <tim.havelka@gmail.com>
2014-12-20 12:30:16 -0600

    status option permissions

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -66,7 +66,7 @@ class StatusesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_status
-      @status = current_user.statuses.find(params[:id])
+      @status = Status.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
